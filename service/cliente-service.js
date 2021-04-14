885d9058867524c22732bc0ff239cbeae82f18de
Marcos <marcos.mpmf43@gmail.com>
2021-04-14 13:38:40 -0300

    adicionando produtos

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,21 +1,3 @@
-const criaNovaLinha = (nome, email, id) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td>
-    `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
-}
-
-const tabela = document.querySelector('[data-tabela]')
-
 const listaClientes = () => {
     return fetch(`http://localhost:3000/profile`)
     .then (resposta => {
@@ -23,8 +5,24 @@ const listaClientes = () => {
     })
 }
 
-listaClientes()
-.then(data => {
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
-})})
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then (resposta => {
+        return resposta.body
+    })
+
+}
+
+export const clienteService = {
+    listaClientes, 
+    criaCliente
+}
